bce491dd60c5dec8bb62f7ff1ec9ea1b1d35cea4
G-dH <georgdh@gmail.com>
2022-05-27 13:00:41 +0200

    extension.js cleanup

--- extension.js
+++ extension.js
@@ -3,24 +3,11 @@
 'use strict';
 
 const { GLib } = imports.gi;
-const Main = imports.ui.main;
-const WorkspaceSwitcherPopup = imports.ui.workspaceSwitcherPopup;
-const AltTab = imports.ui.altTab;
-const WorkspaceThumbnail = imports.ui.workspaceThumbnail;
-const WindowManager = imports.ui.windowManager;
-
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const Settings = Me.imports.settings;
-const _Util = Me.imports.util;
 const OverviewFeaturePack = Me.imports.overviewFeaturePack;
 
-const shellVersion = Settings.shellVersion;
-
-let gOptions;
 let enableTimeoutId = 0;
-
-
 
 function init() {
     ExtensionUtils.initTranslations();
@@ -31,10 +18,6 @@ function enable() {
         GLib.PRIORITY_DEFAULT,
         700,
         () => {
-            //gOptions = new Settings.MscOptions();
-
-            //gOptions.connect('changed', _updateSettings);
-
             OverviewFeaturePack.activate();
             enableTimeoutId = 0;
             return GLib.SOURCE_REMOVE;
@@ -49,28 +32,4 @@ function disable() {
     }
 
     OverviewFeaturePack.reset();
-    if (gOptions) {
-        gOptions.destroy();
-        gOptions = null;
-    }
 }
-
-//------------------------------------------------------------------------------
-function _updateSettings(settings, key) {
-    switch (key) {
-    }
-}
-
-function debug(message) {
-    const stack = new Error().stack.split('\n');
-
-    // Remove debug() function call from stack.
-    stack.shift();
-
-    // Find the index of the extension directory (e.g. particles@schneegans.github.com) in
-    // the stack entry. We do not want to print the entire absolute file path.
-    const extensionRoot = stack[0].indexOf(Me.metadata.uuid);
-
-    log('[' + stack[0].slice(extensionRoot) + '] ' + message);
-}
-
